6e3e9875c0f85117a8824aa456ee25db8769a1ca
Henry Taby <henry.taby@gmail.com>
2025-02-12 12:17:28 -0400

    add product detail information from context

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,18 +1,34 @@
+import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/16/solid'
+import { ShoppingCartContext } from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
+    const context = useContext(ShoppingCartContext)
+
     return (
-        <aside className="product-detail felx flex-col fixed right-0 border border-black rounded-lg bg-white ">
+        <aside 
+        className={`${context.isProductDetailOpen ? 'flex':'hidden'} product-detail felx flex-col fixed right-0 border border-black rounded-lg bg-white`}
+        >
             <div className='flex justify-between items-center p-6'> 
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <div>
-                <XMarkIcon className='h-6 w-6 text-blue-500 x-mark' />
 
-
-                </div>
+                <button className='cursor-pointer'
+                onClick={()=> context.closeProductDetail()}>
+                    <XMarkIcon className='h-6 w-6 text-blue-500 x-mark' />
+                </button>
 
             </div>
+            <figure className='px-6'>
+                <img className='w-full h-full rounded-lg' 
+                src={context.productToShow.data.image} 
+                alt={context.productToShow.data.title} />
+            </figure>
+            <p className='flex flex-col p-6'>
+                <span className='font-medium text-2xl mb-2'>{context.productToShow.data.price}</span>
+                <span className='font-medium text-md'>{context.productToShow.data.title}</span>
+                <span className='font-light text-sm'>{context.productToShow.data.description}</span>
+            </p>
         </aside>
     )
 }
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,14 +1,30 @@
 import { createContext, useState, useMemo } from 'react'
-
+import PropTypes from 'prop-types'; // Importar PropTypes
 const ShoppingCartContext = createContext()
 
 const ShoppingCartProvider = ({children}) => {
+    // Shopping Cart - Increment quantity
     const [count, setCount] = useState(0)
+    // Product Detail - Open/Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+    // Product Detail - Show Product
+    const [productToShow, setProductToShow] = useState({})
 
     const value = useMemo(() => ({
         count,
-        setCount
-    }), [count]); // Dependencias: count
+        setCount,
+        isProductDetailOpen,
+        openProductDetail,
+        closeProductDetail,
+        productToShow,
+        setProductToShow
+    }), [
+        count,
+        isProductDetailOpen,
+        productToShow
+    ]); // Dependencias: count
 
     return(
         <ShoppingCartContext.Provider value={value}>
@@ -16,5 +32,10 @@ const ShoppingCartProvider = ({children}) => {
         </ShoppingCartContext.Provider>
     )
 }
+
+// Validación de PropTypes
+ShoppingCartProvider.propTypes = {
+    children: PropTypes.node.isRequired, // children debe ser un nodo de React y es obligatorio
+};
 
 export {ShoppingCartProvider , ShoppingCartContext}
