14726c6153f968dda7cfe96231f349f6c2f72503
Henry Taby <henry.taby@gmail.com>
2025-02-12 12:29:45 -0400

    fix product detail when data is empty

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -5,7 +5,7 @@ import './styles.css'
 
 const ProductDetail = () => {
     const context = useContext(ShoppingCartContext)
-
+    
     return (
         <aside 
         className={`${context.isProductDetailOpen ? 'flex':'hidden'} product-detail felx flex-col fixed right-0 border border-black rounded-lg bg-white`}
@@ -19,16 +19,20 @@ const ProductDetail = () => {
                 </button>
 
             </div>
-            <figure className='px-6'>
-                <img className='w-full h-full rounded-lg' 
-                src={context.productToShow.data.image} 
-                alt={context.productToShow.data.title} />
-            </figure>
+            {(context.isProductDetailOpen) && 
+                <figure className='px-6'>
+                    <img className='w-full h-full rounded-lg' 
+                    src={context.productToShow.data.image} 
+                    alt={context.productToShow.data.title} />
+                </figure>
+            }
+            {(context.isProductDetailOpen) && 
             <p className='flex flex-col p-6'>
                 <span className='font-medium text-2xl mb-2'>{context.productToShow.data.price}</span>
                 <span className='font-medium text-md'>{context.productToShow.data.title}</span>
                 <span className='font-light text-sm'>{context.productToShow.data.description}</span>
             </p>
+            }
         </aside>
     )
 }
