0c2227cfd193c0b597282b5e5feb42e70eeafc9d
Henry Taby <henry.taby@gmail.com>
2025-02-11 23:59:04 -0400

    add cards components, and list producto from API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,36 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {  
+  const [items, setItems] = useState(null)
+
+  useEffect(()=>{
+    const fetchData = async () => {
+        try {
+          const response = await fetch("https://api.escuelajs.co/api/v1/products");
+          const data = await response.json();
+          setItems(data);
+        } catch (error) {
+          console.error("Error al obtener los datos:", error);
+        }
+      };
+    fetchData();
+
+  }, []);
+
+
   return (
       <Layout>
         Home
-        <Card />
+        <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+          {
+            items?.map(item =>(
+              <Card key={item.id} data={item} />
+            ))
+          }
+        </div>
+        
       </Layout>
   )
 }
