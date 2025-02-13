ad21f2cba10c8856ab1d210eb0d09b55980072bf
Henry Taby <henry.taby@gmail.com>
2025-02-13 13:46:50 -0400

    add utils, and calculate total order price

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/16/solid'
 import { ShoppingCartContext } from '../../Context'
 import OrderCard from '../OrderCard'
+import { totalPrice } from '../../utils'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -37,6 +38,13 @@ const CheckoutSideMenu = () => {
                         />
                     ))
                 }
+            </div>
+            <div className='px-6'>
+                <p className='flex justify-between items-center'>
+                    <span className='font-light'>Total:</span>
+                    <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
+                </p>
+
             </div>
             
         </aside>
