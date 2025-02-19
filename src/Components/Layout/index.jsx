7c56cde9262aecece2cd0fc707cc341ddfc3db50
Henry Taby <henry.taby@gmail.com>
2025-02-19 15:30:53 -0400

    fix validation on Layout

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import PropTypes from 'prop-types'; // Importar PropTypes
+
 const Layout = ({children}) => {
     return(
         <div className='flex flex-col items-center mt-20'>
@@ -6,4 +9,10 @@ const Layout = ({children}) => {
     )
 
 }
+
+// Validación de PropTypes
+Layout.propTypes = {
+    children: PropTypes.node.isRequired, // children debe ser un nodo de React y es obligatorio
+};
+
 export default Layout
